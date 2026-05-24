794f832917d57edf42bb0462d865ee5e7687c0f6
Mihai <mihaiitarann@gmail.com>
2026-05-25 00:20:48 +0200

    fix: reorganizar variables, encadenar alerts, añadir validaciones y extraer DTOs y ColorHelper

---
+++ GestionPisosCompartidos/GestionPisosCompartidos.Client/Services/ColorHelper.cs
@@ -0,0 +1,42 @@
+﻿using MudBlazor;
+
+namespace GestionPisosCompartidos.Client.Services
+{
+    public static class ColorHelper
+    {
+        public static Color GetEstadoColor(string estado)
+        {
+            return estado switch
+            {
+                "Pagado" => Color.Success,
+                "Pendiente" => Color.Warning,
+                "Rechazado" => Color.Error,
+                _ => Color.Default
+            };
+        }
+
+        public static Color GetPrioridadColor(string prioridad)
+        {
+            return prioridad switch
+            {
+                "Baja" => Color.Info,
+                "Media" => Color.Warning,
+                "Alta" => Color.Error,
+                "Urgente" => Color.Error,
+                _ => Color.Default
+            };
+        }
+
+        public static Color GetEstadoIncidenciaColor(string estado)
+        {
+            return estado switch
+            {
+                "Abierta" => Color.Error,
+                "EnProceso" => Color.Warning,
+                "Resuelta" => Color.Success,
+                "Cerrada" => Color.Default,
+                _ => Color.Default
+            };
+        }
+    }
+}
