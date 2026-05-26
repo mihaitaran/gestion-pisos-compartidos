f411ccb841d78bb7e9ff4fbe2cd557469bf072e0
Mihai <mihaiitarann@gmail.com>
2026-05-27 01:12:34 +0200

    feat: filtros vivienda añadidos a vistas propietarios y cambios clases css

--- GestionPisosCompartidos/GestionPisosCompartidos.Client/DTOs/IncidenciaDTO.cs
+++ GestionPisosCompartidos/GestionPisosCompartidos.Client/DTOs/IncidenciaDTO.cs
@@ -7,6 +7,7 @@
         public string Descripcion { get; set; } = "";
         public string Prioridad { get; set; } = "";
         public string? Estado { get; set; }
+        public int ViviendaId { get; set; }
         public string FechaCreacion { get; set; } = "";
         public ViviendaDTO? Vivienda { get; set; }
         public UsuarioDTO? ReportadaPor { get; set; }
